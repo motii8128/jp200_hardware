2ef912510b77d1f29342624790b2f12d1265d5b9
motii <108280115+motii8128@users.noreply.github.com>
2023-11-12 14:03:06 +0900

    Add files via upload

---
+++ include/ros2_jp200_driver/jp200_hardware.hpp
@@ -0,0 +1,29 @@
+#include "rclcpp/rclcpp.hpp"
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/system_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+
+#include <vector>
+
+using hardware_interface::CallbackReturn;
+using hardware_interface::return_type;
+
+namespace jp200_driver
+{
+    class Jp200Driver : public hardware_interface::SystemInterface
+    {
+        public:
+            RCLCPP_SHARED_PTR_DEFINITIONS(Jp200Driver);
+            unsigned int main_loop_update_rate_, desired_hardware_update_rate_ = 100; 
+            CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
+            CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+            return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
+    };
+
+    struct Control
+    {
+        /* data */
+    };
+    
+}

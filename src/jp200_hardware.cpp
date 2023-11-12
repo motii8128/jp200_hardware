364921bcb8c3eb45f0f135a0ec6686da0e9ca76a
motii <108280115+motii8128@users.noreply.github.com>
2023-11-12 15:26:37 +0900

    Add files via upload

---
+++ src/jp200_hardware.cpp
@@ -0,0 +1,17 @@
+#include "ros2_jp200_driver/jp200_hardware.hpp"
+
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+
+
+namespace jp200_driver
+{
+    CallbackReturn Jp200Driver::on_init(const hardware_interface::HardwareInfo & info)
+    {
+        RCLCPP_DEBUG(rclcpp::get_logger("JP200_driver"), "configure");
+        if(hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS)
+        {
+            return CallbackReturn::ERROR;
+        }
+    }
+}

04334ffb6f737e4abd5395c20fa340b4d1276930
motii <108280115+motii8128@users.noreply.github.com>
2023-11-17 17:13:50 +0900

    Add files via upload

---
+++ include/jp200_hardware/port_handler.hpp
@@ -0,0 +1,60 @@
+#ifndef PORT_HANDLER_HPP_
+#define PORT_HANDLER_HPP_
+
+#include <string>
+
+class PortHandler
+{
+    private:
+        int socket_fd_;
+        int baudrate_;
+        std::string port_name_;
+
+        double packet_start_time_;
+        double packet_timeout_;
+        double tx_time_per_bytes;
+
+        bool setupPort(const int c_flag_baud);
+        bool setCustomBaudrate(int speed);
+        int getcFlagBaud(const int baudrate);
+
+        double getCurrentTime();
+        double getTimeSinceStart();
+
+    public:
+        static const int default_baudrate = 115200;
+        
+        static PortHandler getPortHandler(const std::string port_name);
+
+        bool is_using_;
+
+        virtual ~PortHandler(){closePort();};
+
+        bool openPort();
+
+        void closePort();
+
+        void clearPort();
+
+        void setPortName(const std::string port_name);
+
+        std::string getPortName();
+
+        bool setBaudrate(const int baudrate);
+
+        int getBaudrate();
+
+        int getBytesAvailable();
+
+        int readPort(uint8_t *buffer, int length);
+
+        int writePort(uint16_t *buffer, int length);
+
+        void setPacketTimeout(uint16_t packet_length);
+        
+        void setPacketTimeout(double msec);
+
+        bool isPacketTimeout();
+};
+
+#endif 

8f55de08d02a0bca0c1573fd27fec92c006aac6c
motii <108280115+motii8128@users.noreply.github.com>
2023-11-17 17:23:22 +0900

    Add files via upload

--- src/port_handler.cpp
+++ src/port_handler.cpp
@@ -11,6 +11,16 @@
 #include <sys/ioctl.h>
 #include <linux/serial.h>
 
+struct termios2 {
+  tcflag_t c_iflag;       
+  tcflag_t c_oflag;       
+  tcflag_t c_cflag;       
+  tcflag_t c_lflag;       
+  cc_t c_line;            
+  cc_t c_cc[19];          
+  speed_t c_ispeed;       
+  speed_t c_ospeed;       
+};
 
 bool PortHandler::openPort()
 {
@@ -149,3 +159,52 @@ bool PortHandler::setupPort(int c_flag_baud)
     return true;
 }
 
+bool PortHandler::setCustomBaudrate(int speed)
+{
+    struct termios2 options;
+}
+
+int PortHandler::getcFlagBaud(const int baudrate)
+{
+  switch(baudrate)
+  {
+    case 9600:
+      return B9600;
+    case 19200:
+      return B19200;
+    case 38400:
+      return B38400;
+    case 57600:
+      return B57600;
+    case 115200:
+      return B115200;
+    case 230400:
+      return B230400;
+    case 460800:
+      return B460800;
+    case 500000:
+      return B500000;
+    case 576000:
+      return B576000;
+    case 921600:
+      return B921600;
+    case 1000000:
+      return B1000000;
+    case 1152000:
+      return B1152000;
+    case 1500000:
+      return B1500000;
+    case 2000000:
+      return B2000000;
+    case 2500000:
+      return B2500000;
+    case 3000000:
+      return B3000000;
+    case 3500000:
+      return B3500000;
+    case 4000000:
+      return B4000000;
+    default:
+      return -1;
+  }
+}

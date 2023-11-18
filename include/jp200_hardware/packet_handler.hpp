ec26a9c2fb7db2f5649e4d06a8c295f984814f0a
motii <108280115+motii8128@users.noreply.github.com>
2023-11-18 15:38:33 +0900

    Add files via upload

--- include/jp200_hardware/packet_handler.hpp
+++ include/jp200_hardware/packet_handler.hpp
@@ -33,6 +33,23 @@ namespace jp200_hardware
             virtual int ID(HandlerBase *port, uint8_t id, uint16_t *model_number, uint8_t *err = 0) = 0;
 
             virtual int broadcastID(HandlerBase *port, std::vector<uint8_t> &id_list) = 0;
+
+            virtual int action(HandlerBase *port, uint8_t id) = 0;
+
+            virtual int reboot(HandlerBase *port, uint8_t id, uint8_t *err = 0) = 0;
+
+            virtual int clearMultiTurn(HandlerBase *port, uint8_t id, uint8_t *err = 0) = 0;
+
+            virtual int factoryReset(HandlerBase *port, uint8_t id, uint8_t option = 0, uint8_t *err = 0) = 0;
+
+            virtual int readTx(HandlerBase *port, uint8_t id, uint16_t addr, uint16_t length) = 0;
+
+            virtual int readRx(HandlerBase *port, uint8_t id, uint16_t length, uint8_t *data, uint8_t *err = 0) = 0;
+
+            virtual int readTxRx(HandlerBase *port, uint8_t id, uint16_t addr, uint16_t length, uint8_t *data, uint8_t *err = 0) = 0;
+
+            virtual int read_1ByteTx(HandlerBase *port, uint8_t id, uint16_t addr) = 0;
+
     };
 }
 

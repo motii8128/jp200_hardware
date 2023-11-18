0b5f2ca6c170244f292fffe2fe388b496a346dbd
motii <108280115+motii8128@users.noreply.github.com>
2023-11-18 16:47:57 +0900

    Add files via upload

--- src/packet_handler.cpp
+++ src/packet_handler.cpp
@@ -8,16 +8,40 @@ namespace jp200_hardware
 
     PacketHandler::PacketHandler(){}
 
-    std::string PacketHandler::getTxRxResult(int result)
+    const std::string PacketHandler::getTxRxResult(int result)
     {
         switch (result)
         {
         case COMM_SUCCESS:
-            /* code */
-            break;
+            return "[TxRxResult] Communication success.";
+
+        case COMM_PORT_BUSY:
+            return "[TxRxResult] Port is in use!!";
+
+        case COMM_TX_FAIL:
+            return "[TxRxResult] Failed transmit cmd packet!!";
+
+        case COMM_RX_FAIL:
+            return "[TxRxResult] Failed get status packet from SERVO!!";
+
+        case COMM_TX_ERROR:
+            return "[TxRxResult] Incorrect cmd packet!!";
+
+        case COMM_RX_WAITING:
+            return "[TxRxResult] Now recieving status from SERVO!!";
+
+        case COMM_RX_TIMEOUT:
+            return "[TxRxResult] There is no status packet!!";
+
+        case COMM_RX_CORRUPT:
+            return "[TxRxResult] Incorrect cmd packet!!";
+
+        case COMM_NOT_AVAILABLE:
+            return "[TxRxResult] Can't open serial port!!";
+
         
         default:
-            break;
+            return "";
         }
     }
 }

41267901305093245192f84808a8e89bfed639df
motii <108280115+motii8128@users.noreply.github.com>
2023-11-19 10:28:58 +0900

    Add files via upload

--- src/packet_handler.cpp
+++ src/packet_handler.cpp
@@ -91,6 +91,85 @@ namespace jp200_hardware
         uint8_t rx_length = 0;
         uint8_t wait_length = 6;
 
-        
+        while(true)
+        {
+            rx_length += port->readPort(&rx_packet[rx_length], wait_length - rx_length);
+            if(rx_length >= wait_length)
+            {
+                uint8_t idx = 0;
+
+                for(idx = 0; idx < (rx_length - 1); idx++)
+                {
+                    if(rx_packet[idx] == 0xff && rx_packet[idx+1] == 0xff)
+                    {
+                        break;
+                    }
+                }
+
+                if(idx == 0)
+                {
+                    // error check
+                    if(rx_packet[PKT_ID] > 0xFD || rx_packet[PKT_LENGTH] > RXPACKET_MAX_LEN || rx_packet[PKT_ERROR] > 0x7F)
+                    {
+                        for(uint16_t s = 0; s < rx_length-1; s++)
+                        {
+                            rx_packet[s] = rx_packet[s + 1];
+                        }
+                        rx_length -= 1;
+                        continue;
+                    }
+
+                    if(wait_length != rx_packet[PKT_LENGTH] + PKT_LENGTH + 1)
+                    {
+                        wait_length = rx_packet[PKT_LENGTH] + PKT_LENGTH + 1;
+                        continue;
+                    }
+
+                    if(rx_length < wait_length)
+                    {
+                        if(port->isPacketTimeout() == true)
+                        {
+                            if(rx_length == 0)
+                            {
+                                result = COMM_RX_TIMEOUT;
+                            }
+                            else
+                            {
+                                result = COMM_RX_CORRUPT;
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+
+                    for(uint16_t i = 2; i < wait_length - 1; i++)
+                    {
+                        checksum += rx_packet[i];
+                    }
+
+                    checksum = ~checksum;
+
+                    if(rx_packet[wait_length - 1] == checksum)
+                    {
+                        result = COMM_SUCCESS;
+                    }
+                    else
+                    {
+                        result = COMM_RX_CORRUPT;
+                    }
+                    break;
+                }
+                else
+                {
+                    for(uint16_t s = 0; s < rx_length; s++)
+                    {
+                        
+                    }
+                }
+            }
+        }
     }
 }

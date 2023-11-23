b5096a58bd2e1cb8ce476594e85c2e0745716ad7
motii <108280115+motii8128@users.noreply.github.com>
2023-11-23 15:33:21 +0900

    Add files via upload

--- src/jp200_utils.cpp
+++ src/jp200_utils.cpp
@@ -4,8 +4,6 @@ namespace jp200_hardware
 {
     void jp200Utils::setTargetAngle(JP200Cmd cmd, std::string *packet)
     {
-        if(cmd.angle.enable)
-        {
             auto target_angle = std::to_string(cmd.angle.value * 100.0);
             *packet += "TA=" + target_angle;
             if(cmd.angle.trajectory != 0.0)
@@ -18,24 +16,46 @@ namespace jp200_hardware
                 auto target_transition_time = std::to_string(cmd.angle.transition_time);
                 *packet += ";" + target_transition_time;
             }
-        }
     }
-    void jp200Utils::setTargetAngle(JP200Cmd cmd, std::string *packet)
+    void jp200Utils::setTargetVelocity(JP200Cmd cmd, std::string *packet)
     {
-        if(cmd.angle.enable)
-        {
-            auto target_angle = std::to_string(cmd.angle.value * 100.0);
-            *packet += "TA=" + target_angle;
-            if(cmd.angle.trajectory != 0.0)
+            auto target_velocity = std::to_string(cmd.velocity.value * 1000.0);
+            *packet += "TV=" + target_velocity;
+            if(cmd.velocity.trajectory != 0.0)
             {
-                auto target_tajectory = std::to_string(cmd.angle.trajectory);
+                auto target_tajectory = std::to_string(cmd.velocity.trajectory);
                 *packet += ";" + target_tajectory;
             }
-            if(cmd.angle.transition_time != 0.0)
+            if(cmd.velocity.transition_time != 0.0)
             {
-                auto target_transition_time = std::to_string(cmd.angle.transition_time);
+                auto target_transition_time = std::to_string(cmd.velocity.transition_time);
                 *packet += ";" + target_transition_time;
             }
-        }
+    }
+    void jp200Utils::setTargetCurrent(JP200Cmd cmd, std::string *packet)
+    {
+            auto target_current = std::to_string(cmd.current.value);
+            *packet += "TC=" + target_current;
+            if(cmd.current.trajectory != 0.0)
+            {
+                auto target_tajectory = std::to_string(cmd.current.trajectory);
+                *packet += ";" + target_tajectory;
+            }
+            if(cmd.current.transition_time != 0.0)
+            {
+                auto target_transition_time = std::to_string(cmd.current.transition_time);
+                *packet += ";" + target_transition_time;
+            }
+    }
+    void jp200Utils::setPWM(JP200Cmd cmd, std::string *packet)
+    {
+            auto target_pwm = std::to_string(cmd.pwm_rate);
+            *packet += "TP=" + target_pwm;
+    }
+    void jp200Utils::setGetStateEnable(JP200Cmd cmd, std::string *packet)
+    {
+        if(cmd.angle.get_state)*packet += "CA";
+        if(cmd.velocity.get_state)*packet += "CV";
+        if(cmd.current.get_state)*packet += "CC";
     }
 }

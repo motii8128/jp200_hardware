f91eaa615ccc19054c55dadf212a1e2e64cf24d9
motii <108280115+motii8128@users.noreply.github.com>
2023-11-23 15:26:42 +0900

    Add files via upload

--- src/jp200_utils.cpp
+++ src/jp200_utils.cpp
@@ -6,8 +6,36 @@ namespace jp200_hardware
     {
         if(cmd.angle.enable)
         {
-            auto value_str = std::to_string(cmd.angle.value);
-            *packet += "TA=" + value_str;
+            auto target_angle = std::to_string(cmd.angle.value * 100.0);
+            *packet += "TA=" + target_angle;
+            if(cmd.angle.trajectory != 0.0)
+            {
+                auto target_tajectory = std::to_string(cmd.angle.trajectory);
+                *packet += ";" + target_tajectory;
+            }
+            if(cmd.angle.transition_time != 0.0)
+            {
+                auto target_transition_time = std::to_string(cmd.angle.transition_time);
+                *packet += ";" + target_transition_time;
+            }
+        }
+    }
+    void jp200Utils::setTargetAngle(JP200Cmd cmd, std::string *packet)
+    {
+        if(cmd.angle.enable)
+        {
+            auto target_angle = std::to_string(cmd.angle.value * 100.0);
+            *packet += "TA=" + target_angle;
+            if(cmd.angle.trajectory != 0.0)
+            {
+                auto target_tajectory = std::to_string(cmd.angle.trajectory);
+                *packet += ";" + target_tajectory;
+            }
+            if(cmd.angle.transition_time != 0.0)
+            {
+                auto target_transition_time = std::to_string(cmd.angle.transition_time);
+                *packet += ";" + target_transition_time;
+            }
         }
     }
 }
